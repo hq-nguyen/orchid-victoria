65bb18b62e4305cda1e8263269e702211056e9ce
hq-nguyen <quannvhqe180068@fpt.edu.vn>
2025-02-09 17:17:29 +0700

    lab 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
 import Navbar from './component/Navbar/Navbar'
+import OrchidSection from './component/OrchidSection/OrchidSection'
 
 function App() {
 
   return (
     <div>
       <Navbar />
+      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] dark:bg-gray-900'>
+        <OrchidSection />
+        {/* <OrchidPresentation /> */}
+      </div>
     </div>
   )
 }
